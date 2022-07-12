4def120003519f9a9d51b8d85f90fc1afe180ec8
Lukas Mills <lukascodes64@gmail.com>
2022-07-12 10:54:12 -0400

    MVP

--- script.js
+++ script.js
@@ -2,20 +2,43 @@ fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=American")
 .then(res => res.json())
 .then(data => data.meals.forEach(getMeals))
 
-const para = document.getElementById("pics")
+const recipeContainer = document.getElementById("recipe-container")
 
 function getMeals (meals) {
+    
     const mealName = document.createElement("p")
     const images = document.createElement("img")
+    const recipeButton = document.createElement('button')
+    const commentSection = document.createElement('form')
+    const inputComment = document.createElement('input')
+    const submitComment = document.createElement('input')
 
-    mealName.style.color = "white"
+    
+
+    
+
+    
 
     mealName.innerHTML = meals.strMeal
+    mealName.style.color = "white"
     images.src = meals.strMealThumb
+    images.style = "width: 100px"
+    recipeButton.innerText = 'Like'
+    commentSection.innerText = 'Add Comment'
+    commentSection.style.color ="white"
+    inputComment.type = "text"
+    submitComment.type = 'submit'
+    
+    
+    
 
     // console.log(meals);
-    para.append(mealName)
-    para.append(images)
+    recipeContainer.append(mealName)
+    recipeContainer.append(images)
+    recipeContainer.append(recipeButton)
+    recipeContainer.append(commentSection)
+    commentSection.append(inputComment)
+    commentSection.append(submitComment)
 
 
 }
