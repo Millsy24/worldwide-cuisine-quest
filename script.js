8d2c4d2093cae73dc54d0bf76108a21827449a85
Lukas Mills <lukascodes64@gmail.com>
2022-07-12 18:22:36 -0400

    Appeneded images to grid

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=American`)
 .then(data => data.meals.forEach(getMeals))
 
 
-const recipeContainer = document.getElementById(`recipe-container`)
+const recipeContainer = document.querySelector(`.recipe-container`)
 
 
 function getMeals (meals) {
@@ -11,8 +11,15 @@ function getMeals (meals) {
 
 
 const recipeCard = document.createElement(`div`)
+const recipeImageContainer = document.createElement(`div`)
 const recipeHeader = document.createElement(`h2`)
+const recipeFooter = document.createElement(`footer`)
+const reicpeButton = document.createElement(`button`)
 const recipeImage = document.createElement(`img`)
+
+    
+
+
 
 
 
@@ -22,7 +29,11 @@ const recipeImage = document.createElement(`img`)
 
 
 recipeCard.classList = `recipe`
-
+recipeImage.classList = 'recipe-header recipe-image'
+recipeHeader.classList = `recipe-body`
+recipeFooter.classList = `recipe-footer`
+reicpeButton.classList = `recipe-button`
+reicpeButton.innerText = `recipe`
 recipeHeader.innerText = meals.strMeal
 recipeImage.src = meals.strMealThumb
 
@@ -35,8 +46,12 @@ recipeImage.src = meals.strMealThumb
 
 
 recipeContainer.append(recipeCard)
+recipeCard.append(recipeImageContainer)
+recipeImageContainer.append(recipeImage)
 recipeCard.append(recipeHeader)
-recipeCard.append(recipeImage)
+recipeCard.append(recipeFooter)
+recipeCard.append(reicpeButton)
+
 
 }
 
