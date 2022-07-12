a493e35c491e300c6fb43c0fd211aecc151ac8ca
Lukas Mills <lukascodes64@gmail.com>
2022-07-12 17:33:00 -0400

    Appended images and header

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=American")
+fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=American`)
 .then(res => res.json())
 .then(data => data.meals.forEach(getMeals))
 
-const recipeContainer = document.getElementById("recipe-container")
+
+const recipeContainer = document.getElementById(`recipe-container`)
+
 
 function getMeals (meals) {
-    
-    const mealName = document.createElement("p")
-    const images = document.createElement("img")
-    const recipeButton = document.createElement('button')
-    const commentSection = document.createElement('form')
-    const inputComment = document.createElement('input')
-    const submitComment = document.createElement('input')
 
-    
 
-    
 
-    
+const recipeCard = document.createElement(`div`)
+const recipeHeader = document.createElement(`h2`)
+const recipeImage = document.createElement(`img`)
 
-    mealName.innerHTML = meals.strMeal
-    mealName.style.color = "white"
-    images.src = meals.strMealThumb
-    images.style = "width: 100px"
-    recipeButton.innerText = 'Like'
-    commentSection.innerText = 'Add Comment'
-    commentSection.style.color ="white"
-    inputComment.type = "text"
-    submitComment.type = 'submit'
-    
-    
-    
 
-    // console.log(meals);
-    recipeContainer.append(mealName)
-    recipeContainer.append(images)
-    recipeContainer.append(recipeButton)
-    recipeContainer.append(commentSection)
-    commentSection.append(inputComment)
-    commentSection.append(submitComment)
 
+
+
+
+
+
+recipeCard.classList = `recipe`
+
+recipeHeader.innerText = meals.strMeal
+recipeImage.src = meals.strMealThumb
+
+
+
+
+
+
+
+
+
+recipeContainer.append(recipeCard)
+recipeCard.append(recipeHeader)
+recipeCard.append(recipeImage)
 
 }
+
+
+
